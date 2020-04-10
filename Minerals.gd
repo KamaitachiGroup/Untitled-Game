ae4e201121d658acab65c43929e300c3e068d372
antoine.maillard@epitech.eu <antoine.maillard@epitech.eu>
2020-04-10 15:24:03 +0200

    minerals update ok

--- Minerals.gd
+++ Minerals.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 
 # Declare member variables here. Examples:
-enum queueBroken {
+enum {
 	NODE,
 	PASSED_TIME,
 	RESPAWN_TIME,
@@ -18,17 +18,18 @@ var thread
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scene = get_tree().get_root()
+	loadStateMinerals()
 	thread = Thread.new()
 	thread.start(self, "respawnMinerals", listBroken)
 
 func respawnMinerals(listBroken):
 	while (true):
 		for i in range(0, listBroken.size()):
-			if (listBroken[i][queueBroken.STATE] == true
-				and OS.get_ticks_msec() - listBroken[i][queueBroken.PASSED_TIME] >= listBroken[i][queueBroken.RESPAWN_TIME]
-				and is_instance_valid(listBroken[i][queueBroken.NODE])):
-					listBroken[i][queueBroken.NODE].visible = true
-					listBroken[i][queueBroken.STATE] = false
+			if (listBroken[i][STATE] == true
+				and OS.get_ticks_msec() - listBroken[i][PASSED_TIME] >= listBroken[i][RESPAWN_TIME]
+				and is_instance_valid(listBroken[i][NODE])):
+					listBroken[i][NODE].visible = true
+					listBroken[i][STATE] = false
 					listBroken.remove(i)
 					i -= 1
 
@@ -41,3 +42,32 @@ func _unhandled_input(event):
 			if mineral != null and mineral.visible == true:
 				listBroken.push_back([mineral, OS.get_ticks_msec(), 30000, true])
 				mineral.visible = false
+
+func saveStateMinerals():
+	var save = File.new()
+	save.open("user://stateMinerals.save", File.WRITE)
+	for i in range(0, listBroken.size()):
+		if listBroken[i][STATE] == true:
+			listBroken[i][RESPAWN_TIME] -= OS.get_ticks_msec() - listBroken[i][PASSED_TIME]
+			listBroken[i][NODE] = listBroken[i][NODE].name
+	save.store_line(to_json(listBroken))
+	save.close()
+	
+func loadStateMinerals():
+	var loadF = File.new()
+	if not loadF.file_exists("user://stateMinerals.save"):
+		return
+	loadF.open("user://stateMinerals.save", File.READ)
+	while loadF.get_position() < loadF.get_len():
+		listBroken = parse_json(loadF.get_line())
+	for i in range(0, listBroken.size()):
+		if listBroken[i][STATE] == true:
+			listBroken[i][NODE] = scene.get_node("Minerals/" + listBroken[i][NODE])
+			listBroken[i][NODE].visible = false
+	print(listBroken)
+	loadF.close()
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		saveStateMinerals()
+		get_tree().quit()
